aac8e1c6d97d63ce016be43315611087d25f52f5
Godswill Udoh <godswilludoh@gmail.com>
2022-02-23 21:28:31 +0100

    refactored the create-react-app, added assets and components pages, added signup and welcome files

---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp = () => {
+	return (
+		<div>
+			<div className='images'></div>
+			<div className='form'></div>
+		</div>
+	);
+};
+
+export default SignUp;
