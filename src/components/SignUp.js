f8055e9debaef14c328d8339a31c4259f7df108a
Godswill Udoh <godswilludoh@gmail.com>
2022-02-23 21:54:21 +0100

    added content to the signup page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Images from './../assets/images.png';
 
 const SignUp = () => {
 	return (
 		<div>
-			<div className='images'></div>
-			<div className='form'></div>
+			<div className='images'>
+				<img src={Images} alt='customer' />
+			</div>
+			<div className='form'>
+				<label>Sign up</label>
+				<div className='form-input'>
+					<label>Email</label>
+					<input type='email' />
+					<label>Password</label>
+					<input type='password' />
+				</div>
+				<button>Sign up</button>
+			</div>
+			<h3>or sign up using </h3>
 		</div>
 	);
 };
